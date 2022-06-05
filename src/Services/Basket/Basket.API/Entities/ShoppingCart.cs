cad9f315a7c7c9f3f517136785f077d71331e4de
moeen farhat <moeen.farhat@gmail.com>
2022-06-05 16:58:44 +0400

    backet micro

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items { get; set; } = new List<ShoppingCartItems>();
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
